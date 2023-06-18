3b944532be46a56043bd50224562e79d7ba1d036
id_17 <2020302111121@whu.edu.cn>
2023-06-18 23:44:55 +0800

    errbook generator

--- ErrorBookGenerate.py
+++ ErrorBookGenerate.py
@@ -5,7 +5,6 @@ import os
 Files = os.popen("git diff --name-only HEAD HEAD^").read().split("\n")
 #get time as MONTH-DAY
 time = datetime.datetime.now().strftime("%m%d")
-print(time)
 for file in Files:
     os.system(f"diff -y --suppress-common-lines ref/{file} test/{file} > err/{file}.{time}.txt")
 # os.system(f"git add err && git commit -m \"update {time}'s error books\" && git push")
