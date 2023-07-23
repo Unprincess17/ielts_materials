87005c6b28acf26bbf0b3d4aca33eb3844d65adb
id_17 <2020302111121@whu.edu.cn>
2023-07-23 19:55:43 +0800

    refactor: 将听力仓库修改为存放所有物料的仓库

--- listening/ErrorBookGenerate.py
+++ listening/ErrorBookGenerate.py
@@ -6,8 +6,8 @@ Files = os.popen("git diff --name-only HEAD HEAD^").read().split("\n")
 #get time as MONTH-DAY
 time = datetime.datetime.now().strftime("%m%d")
 for file in Files:
-    if(os.path.exists(file) and file.startswith("test")):
+    if(os.path.exists(file) and file.startswith("listening/test")):
         filename = os.path.basename(file)
         print(filename)
-        os.system(f"diff --ignore-space-change -y --suppress-common-lines ref/{filename} test/{filename} > err/{filename}.{time}.txt")
+        os.system(f"diff --ignore-space-change -y --suppress-common-lines listening/ref/{filename} listening/test/{filename} > listening/err/{filename}.{time}.txt")
 # os.system(f"git add err && git commit -m \"update {time}'s error books\" && git push")
---
+++ speaking/speaking.md
@@ -0,0 +1,20 @@
+# Part2 Monologue
+2 minutes
+## topics for part 2
+- people
+- places
+- events
+- objects
+
+# Part3
+> Comparing: Do you think men and women have the same bohbies?
+> Predict: What will the houses be like in the future
+> Explaination: Why do people dislike lending things to others
+> Pros and Cons: What are the advantages and disadvantages of online courses
+
+
+# 话题延伸方法
+- 分类分情况
+  - 有时间/没时间
+  - 人群：老少/男女
+  - 大多数情况/例外
